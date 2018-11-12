14e1e8a5a8e977784ff2546d1d040fa9dd5185ee
Parkour <1483724439@qq.com>
2018-11-12 19:54:11 +0800

    添加修改用户和扔瓶子

--- dao.go
+++ dao.go
@@ -1,6 +1,10 @@
 package main
 
-import "mtcomm/db/mysql"
+import (
+	"mtcomm/db/mysql"
+	"bytes"
+	"github.com/golang/go/src/pkg/fmt"
+)
 
 type Request struct {
 	Status  int        `json:"status"`
@@ -26,4 +30,20 @@ type RequestMap struct {
 func i_user(r *RequestMap) error {
 	sql := "INSERT INTO `user` SET `user_id`=?,`sex`=?,`nickname`=?,`phone`=?,`province`=?,`city`=?,`area`=?,`sign`=?,createTime=NOW(),`updateTime`=NOW() ON DUPLICATE KEY UPDATE `sex`=?,`nickname`=?,`phone`=?,`province`=?,`city`=?,`area`=?,`sign`=?,`updateTime`=NOW()"
 	return mysqlClient.Execute(&mysql.Stmt{Sql: sql, Args: []interface{}{r.Id, r.Sex, r.Nickname, r.Phone, r.Province, r.City, r.Area, r.Sign, r.Sex, r.Nickname, r.Phone, r.Province, r.City, r.Area, r.Sign}})
+}
+
+func s_user(r *RequestMap) ([]map[string]string, error) {
+	sqlBuffer := bytes.Buffer{}
+	sqlBuffer.WriteString("select * from `user` where 1=1")
+	if r.Id != 0 {
+		sqlBuffer.WriteString(" and id='")
+		sqlBuffer.WriteString(fmt.Sprint(r.Id))
+		sqlBuffer.WriteString("'")
+	}
+	sql := sqlBuffer.String()
+	result, err := mysqlClient.SearchMutiRows(&mysql.Stmt{Sql: sql, Args: []interface{}{}})
+	if err != nil {
+		return result, err
+	}
+	return result, nil
 }
